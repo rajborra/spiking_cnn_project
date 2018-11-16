1acf4458f3b0d76cdf010feaaf7853320173426b
rajborra <rkb15@duke.edu>
2018-11-15 21:17:24 -0500

    added framework for epoch iteration

--- finalproj503.py
+++ finalproj503.py
@@ -18,42 +18,59 @@ neurons = NeuronGroup(num_neurons, eqs, clock = Clock(defaultclock.dt), threshol
 neurons.v = -60
 neurons.J = 0
 monitor_v = StateMonitor(neurons,'v',record=True)
+# monitr_spike = SpikeMonitor(neurons,,
 monitor_J = StateMonitor(neurons,'J',record=True)
 
-## Synapses
+## Synapse Initialization
 layer1 = [0,1]
 layer2 = [2,3,4,5]
 layer3 = [6]
-w = [random.random()-0.5 for _ in range(8)]
+w = [random.random()-0.5 for _ in range(12)]
 
-syns = Synapses(neurons, neurons, clock = Clock(defaultclock.dt), model = 'weight:1', on_pre = 'J += weight')
-syns.connect(i=[0,0,0,0,1,1,1,1],j=[2,3,4,5,2,3,4,5])
+syns = Synapses(neurons, neurons, clock = Clock(defaultclock.dt), model = 'weight:1', on_pre = 'J += 25*weight')
+syns.connect(i=[0,0,0,0,1,1,1,1,2,3,4,5],j=[2,3,4,5,2,3,4,5,6,6,6,6])
 syns.weight = w
 
 ## Simulation
+num_trials = 5
+data_set = [[0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0]]
+
 neurons.J[0] = 0
 run(5*ms)
-neurons.J[0] = 50
-run(25*ms)
-neurons.J[0] = 50
-w[3] = -1*w[3]
-syns.weight = w
-run(50*ms)
+for _ in range(num_trials):
+    for set in data_set:
+        # Set Spiking Rate for Input Neurons
+        neurons.J[0] = set[0]*50
+        neurons.J[1] = set[1]*50
+        run(25*ms)
+
+        # Calculate Errors
+        # err = 
+
+        # Reset Current to Input Neurons
+        neurons.J[0] = 0
+        neurons.J[1] = 0
+        run(5*ms)
 
 ## Plotting
 figure(1)
-subplot(6,1,1)
+subplot(7,1,1)
 plot(monitor_v.t/ms, monitor_v.v[0])
-subplot(6,1,2)
+subplot(7,1,2)
 plot(monitor_v.t/ms, monitor_v.v[1])
-subplot(6,1,3)
+subplot(7,1,3)
 plot(monitor_J.t/ms, monitor_J.J[2])
-subplot(6,1,4)
+subplot(7,1,4)
 plot(monitor_J.t/ms, monitor_J.J[3])
-subplot(6,1,5)
+subplot(7,1,5)
 plot(monitor_J.t/ms, monitor_J.J[4])
-subplot(6,1,6)
+subplot(7,1,6)
 plot(monitor_J.t/ms, monitor_J.J[5])
+subplot(7,1,7)
+plot(monitor_v.t/ms, monitor_v.v[6])
 xlabel('Time (ms)')
-xlim(0,50)
+# xlim(0,50)
 show()
